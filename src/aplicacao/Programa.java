f878ab480e37b45bd62b2b03c14926039a4b57ff
Alexandra Parra <diana00suarez@gmail.com>
2022-02-17 12:27:04 -0300

    Departamento classe entidade

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+import modelo.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Departamento obj = new Departamento(1, "Livros");
+		System.out.println(obj);
+	}
+
+}
---
+++ src/modelo/entidades/Departamento.java
@@ -0,0 +1,57 @@
+package modelo.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departamento implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nome;
+	
+	public Departamento() {
+	}
+
+	public Departamento(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departamento other = (Departamento) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Departamento [id=" + id + ", nome=" + nome + "]";
+	}
+}
