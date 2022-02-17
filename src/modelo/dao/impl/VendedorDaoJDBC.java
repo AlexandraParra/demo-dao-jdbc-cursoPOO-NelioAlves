5a66b2f65fb98601aca7055a9c86134d71313155
Alexandra Parra <diana00suarez@gmail.com>
2022-02-17 16:04:07 -0300

    VendedorDao e DaoFactory

---
+++ src/modelo/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,40 @@
+package modelo.dao.impl;
+
+import java.util.List;
+
+import modelo.dao.VendedorDao;
+import modelo.entidades.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDao{
+
+	@Override
+	public void insert(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vendedor findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
